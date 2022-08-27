ab2cc1adad3d3d69d3bdb4ce30974e1099cb772c
FZfortirex <99706613+FZfortirex@users.noreply.github.com>
2022-08-27 18:37:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,16 +16,16 @@ global.xteam = 'apivproject'
 global.lolhuman = 'yourkey'
 
 // Other
-global.owner = ['6281511480762']
-global.premium = ['6281511480762']
-global.ownername = 'YahyaGanzz'
-global.botname = 'AzzBot•MD'
-global.packname = '© YahyaGanzz'
-global.gc = 'https://github.com/AzzBott679'
-global.linkyt = 'https://youtube.com/channel/UCmUhiytK9WMYaOFgdOho_2g'
-global.linkgc = 'https://chat.whatsapp.com/CkguID8hSdSFAaBrKNHLSr'
+global.owner = ['6285728581059']
+global.premium = ['6285728581059']
+global.ownername = 'Burhanuddin Zain'
+global.botname = 'Cemara BotWhatsapp'
+global.packname = '© Cemara'
+global.gc = 'Nothing.'
+global.linkig = 'https://www.instagram.com/dnzainn/'
+global.linkgc = 'Private.'
 global.limitawal = '10'
-global.author = '@yahyaganzz\n@AzzBot'
+global.author = '@dnzainn'
 global.sessionName = 'sessionya'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⌕'
